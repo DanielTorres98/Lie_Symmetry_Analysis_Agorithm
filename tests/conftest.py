6da461734fcc1bd3ab7d3f28ef6d13ce204590a5
Daniel Torres Valladares <81983942+DanielTorres98@users.noreply.github.com>
2021-09-02 10:16:48 -0500

    Daniel/det eqn (#10)
    * determining equation logic
    
    * first tests added
    
    * docstrings added
    
    * fix string to dict
    
    * New notebook with all the logic

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,6 +7,10 @@ data_is_zero = "data_test/data_algebra/data_is_zero.json"
 
 data_drop_constants = "data_test/data_algebra/data_drop_constants.json"
 
+data_compare_derivatives = "data_test/data_algebra/data_compare_derivatives.json"
+
+data_key_ordering = "data_test/data_algebra/data_key_ordering.json"
+
 def load_test_data(path):
     with open(path) as data_file:
         data = json.load(data_file)
@@ -19,5 +23,15 @@ def is_zero_data_test(request):
 
 @fixture(params=load_test_data(data_drop_constants))
 def drop_constants_data_test(request):
+    data = request.param
+    return data
+
+@fixture(params=load_test_data(data_compare_derivatives))
+def compare_derivatives_data_test(request):
+    data = request.param
+    return data
+
+@fixture(params=load_test_data(data_key_ordering))
+def key_ordering_data_test(request):
     data = request.param
     return data
--- tests/test_alebra_operations.py
+++ tests/test_alebra_operations.py
@@ -1,4 +1,4 @@
-from utils import algebra
+from Notebooks.utils import algebra
 
 def test_is_zero(is_zero_data_test):
     zero_term = {"coefficient": 4,
@@ -11,3 +11,14 @@ def test_is_zero(is_zero_data_test):
 def test_drop_constants(drop_constants_data_test):
     eqn = drop_constants_data_test
     assert eqn["result"] == algebra.drop_constants(eqn["test"])
+
+def test_compare_derivatives(compare_derivatives_data_test):
+    D1 = compare_derivatives_data_test["zero_term"]
+    D2 = compare_derivatives_data_test["term"]
+    assert compare_derivatives_data_test["result"] == algebra.compare_derivatives(D1, D2)
+
+def test_key_ordering(key_ordering_data_test):
+    original_list = key_ordering_data_test["original"]
+    result = key_ordering_data_test["result"]
+    ordered_list = algebra.key_ordering(original_list)
+    assert result == ordered_list
