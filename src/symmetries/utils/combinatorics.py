977bc5eec5fadf98360283a85b0ae2e2caa30d70
Carla Lopez <carlalopez3108@gmail.com>
2022-09-19 23:56:41 -0500

    combinatorics

--- src/symmetries/utils/combinatorics.py
+++ src/symmetries/utils/combinatorics.py
@@ -1,25 +1,25 @@
+"""list_combinatorics function"""
+
 from itertools import combinations_with_replacement as cwr
 
 def list_combinatorics(variables, order):
-    """Given a list of variables returns
-       all possible combinations.
+    """Given a list of variables returns all possible combinations.
 
     Parameters
     ----------
-    variables : [list]
+    variables : list
         A list with the symbolic variables.
 
-    order : [int]
+    order : int
         order of the differential equation
 
     Returns
     -------
-    [list]
-        A list of lists with all possible 
-        combinations
+    list
+        A list of lists with all possible combinations
     """
     var_combinatorics = []
-    for o in range(1, order+1):
-        var_combinatorics = var_combinatorics + list(cwr(variables, o))
+    for order_i in range(1, order+1):
+        var_combinatorics = var_combinatorics + list(cwr(variables, order_i))
     var_combinatorics = [list(x) for x in var_combinatorics]
     return var_combinatorics
