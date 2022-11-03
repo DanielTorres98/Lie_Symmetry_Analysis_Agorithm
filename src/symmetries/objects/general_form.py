5528765552b369570eb1013c72c5ddf77f7d7862
Carla Lopez <carla@climate.ai>
2022-11-03 11:50:05 -0600

    format, docstring and consolidate find_deleted_items

--- src/symmetries/objects/general_form.py
+++ src/symmetries/objects/general_form.py
@@ -1,9 +1,10 @@
 
-from copy import deepcopy
+import sympy
 import regex as re
+from copy import deepcopy
 from .system import System
 from .determining_equations import DeterminingEquations
-from symmetries.utils.symbolic import sym_det_eqn
+from symmetries.utils.symbolic import get_symbolic_terms
 
 
 class GeneralForm():
@@ -14,6 +15,14 @@ class GeneralForm():
         self.deleted = {}
 
     def obtain_general_form(self):
+        """Proposes a general form of solutions for all infinitesimals as a function of
+        all the independent and dependent variables accordingly.
+
+        Returns
+        -------
+        dictionary
+            Keys are the infinitesimals eta and eta and values are list of relevant variables.
+        """
         general_form = {}
         infinitesimals = self.model.infinitesimals_ind + self.model.infinitesimals_dep
 
@@ -27,15 +36,19 @@ class GeneralForm():
         return general_form
 
     def find_first_derivative_equals_0(self):
-        for k, v in self.determining_equations.items():
+        """Finds equations in the determining equations that contain a single term that is a first
+        derivative of a given variable and stores it in a dictionary that contains deleted
+        dependencies called deleted."""
+
+        for v in self.determining_equations.values():
             if len(v) == 1:
                 l = v[0]
                 if sum(l['derivatives']) == 1:
                     var = [self.model.all_variables[n]
-                        for n, d in enumerate(l['derivatives']) if d][0]
+                           for n, d in enumerate(l['derivatives']) if d][0]
                     if not (l['variable'] in self.deleted and var in self.deleted[l['variable']]):
                         self.general_form[l['variable'][:-1]
-                                        ][l['variable'][-1]].remove(var)
+                                          ][l['variable'][-1]].remove(var)
                         if l['variable'] not in self.deleted:
                             self.deleted[l['variable']] = [var]
                             print('deleting', l['variable'], var)
@@ -43,38 +56,65 @@ class GeneralForm():
                             self.deleted[l['variable']].append(var)
                             print('deleting', l['variable'], var)
 
-
     def find_deleted_items_in_equations(self):
+        """Searches in the determining equations for equations that contain a term that is an
+        already identified first derivative equals 0 or higher order or cross derivative of the
+        same."""
+
         for k, v in self.determining_equations.items():
             if len(v) > 1:
                 values = deepcopy(v)
                 for item in values:
                     if item['variable'] in self.deleted:
-                        vars = [self.model.all_variables[n]
-                                for n, d in enumerate(item['derivatives']) if d]
-                        for var in vars:
-                            if var in self.deleted[item['variable']] and item in v:
-                                print('found', var, 'in', item['variable'], 'eq', k)
-                                v.remove(item)
-
-    def delete_second_derivatives(self):
-        det_eqns = deepcopy(self.determining_equations)
-        for k, v in det_eqns.items():
-                values = deepcopy(v)
-                for item in values:
-                    if item['variable'] in self.deleted:
                         for var, order in zip(self.model.all_variables, item['derivatives']):
-                            if var in self.deleted[item['variable']] and order>1:
-                                print('found', var, 'in', item['variable'], 'eq', k)
+                            if var in self.deleted[item['variable']] and order > 0 and item in v:
+                                print('found', var, 'in',
+                                      item['variable'], 'eq', k)
                                 v.remove(item)
-                                if len(v)==0:
+                                if len(v) == 0:
                                     del self.determining_equations[k]
-                            
+
     def print_matrix(self):
-        print('general form:', self.general_form) # pretty print this as sympy
+        print('general form:', self.general_form)  # pretty print this as sympy
         print('already deleted:', self.deleted)
-        return sym_det_eqn(
-            self.determining_equations,
-            self.model.independent_variables,
-            self.model.dependent_variables,
-            self.model.constants)
+        return self.print_symbolic_equations(self.determining_equations)
+
+    def print_symbolic_equations(self, equations):
+        """Gives the symbolic version of remaining
+        determining equation.
+
+        Parameters
+        ----------
+        det_eqn : dict
+            dictionary will all the determining equations.
+        """
+        var_dict = {}
+
+        indep_var_str = [str(ele).replace(' ', '')
+                         for ele in self.model.independent_variables]
+        for dep_var in indep_var_str:
+            var = dep_var.split('(')[0]
+            if len(var) > 1:
+                var_dict[f'xi{var}'] = 'eta^' + \
+                    '(' + "\\" + var + ')'
+            else:
+                var_dict[f'xi{var}'] = f'xi^({var})'
+
+        dep_var_str = [str(ele).replace(' ', '')
+                       for ele in self.model.dependent_variables]
+        for dep_var in dep_var_str:
+            var = dep_var.split('(')[0]
+            if len(var) > 1:
+                var_dict[f'eta{var}'] = 'eta^' + \
+                    '(' + "\\" + var + ')'
+            else:
+                var_dict[f'eta{var}'] = f'eta^({var})'
+
+        matrix = sympy.Matrix([[]])
+        for i, eqn in enumerate(equations.values()):
+            matrix = matrix.row_insert(i,
+                                       sympy.Matrix([[i, sympy.Eq(get_symbolic_terms(
+                                           eqn, var_dict, self.model.constants, self.model.all_variables
+                                       ), 0)]])
+                                       )
+        return matrix
