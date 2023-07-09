0e7db2f4433672776b53571176cd7687aaaee500
Carla Lopez <carla@climate.ai>
2023-07-08 19:04:18 -0500

    format and docstrings

--- src/symmetries/utils/latex.py
+++ src/symmetries/utils/latex.py
@@ -1,19 +1,14 @@
-def dict_to_latex(term, var_dict, var_list, 
-                    sym_cte_list, one_term):
-    """Given a dictionary it returns the symbolic
-       equivalent. It drops all constants if it is 
-       just one term.
+def dict_to_latex(term, var_dict, var_list, sym_cte_list, one_term):
+    """Given a dictionary it returns the symbolic equivalent. It drops all constants if it is  just one term.
 
     Parameters
     ----------
     term : [dict]
         dictionary containing the information of the term
     var_dict : [dict]
-        dictionary translating the variable name to 
-        latex format
-    var_list : list with all variables (independant and 
-        dependant)
-        list with the constants writen in latex format
+        dictionary translating the variable name to latex format
+    var_list : [list]
+        list with all variables (independent and dependant)list with the constants written in latex format
     sym_cte_list : [list]
         [description]
     one_term : [boolean]
@@ -32,37 +27,33 @@ def dict_to_latex(term, var_dict, var_list,
         coeff = ''
     else:
         for cte, n in cte_power:
-            if len(str(cte))>1:
+            if len(str(cte)) > 1:
                 if n == 1:
                     a = a + "\\" + str(cte)
                 if n > 1:
-                    a = a + "\\" + str(cte) + '^' + str(n) 
+                    a = a + "\\" + str(cte) + '^' + str(n)
             else:
                 if n == 1:
                     a = a + str(cte)
                 if n > 1:
-                    a = a + str(cte) + '^' + str(n)           
+                    a = a + str(cte) + '^' + str(n)
         coeff = str(term['coefficient'])
     D = latex_derivative(list_devs, var, var_list)
     latex_term = coeff + a + D
     return latex_term
 
+
 def latex_derivative(list_devs, var, var_list):
-    """Given a list of derivatives executes all the
-       derivatives on the variable.
+    """Given a list of derivatives executes all the derivatives on the variable.
 
     Parameters
     ----------
     list_devs : [list]
-        list of ints containing the 
-        order of the derivative 
-        with respect to the variable
-        var_lists.
+        list of ints containing the order of the derivative with respect to the variable var_lists.
     var : [str]
         variable to be differentiate
     var_list : [list]
-        list of independant and dependant
-                          variables.
+        list of independant and dependant variables.
 
     Returns
     -------
@@ -73,9 +64,9 @@ def latex_derivative(list_devs, var, var_list):
     var_str = '\\' + var
     for D, v in D_v:
         for _ in range(D):
-            if len(str(v))>1:
+            if len(str(v)) > 1:
                 if '_' in var_str:
-                    var_str = var_str + "\\" +  v
+                    var_str = var_str + "\\" + v
                 else:
                     var_str = var_str + '_' + '{' + "\\" + v
             else:
@@ -86,28 +77,25 @@ def latex_derivative(list_devs, var, var_list):
     var = var_str + '}'
     return var
 
-def latex_eqn_code(eqn, var_dict, var_list, constants):
-    """This code generates the latex string format of 
-    the determinant equations
+
+def latex_eqn_code(eqn: dict, var_dict: dict, var_list: list, constants: list) -> str:
+    """This code generates the latex string format of the determinant equations
 
     Parameters
     ----------
     eqn : [dictionary]
-        dictionary containing all the determinant eqautions
+        dictionary containing all the determinant equations
     var_dict : [dictionary]
-        dictionary mapping the name of the variable to it's 
-        latex code 
+        dictionary mapping the name of the variable to its latex code
     var_list : [list]
-        list with all independant and dependant variables in
-        string format
+        list with all independent and dependant variables in string format
     constants : [list]
         list containing all constants
 
     Returns
     -------
     [string]
-        returns a single string with  all the equations 
-        typed in latex format
+        single string with all the equations typed in latex format
     """
     sym_cte_list = []
     for idx in range(len(var_list)):
@@ -120,22 +108,33 @@ def latex_eqn_code(eqn, var_dict, var_list, constants):
         if len(eqn) == 1:
             one_term = True
         A = A + dict_to_latex(term, var_dict, var_list,
-                                 sym_cte_list, one_term) + '+'
+                              sym_cte_list, one_term) + '+'
     A = A[:-1]
     A = A + '=0'
     return A
 
 
 def latex_det_eqn(det_eqn, var_dict, var_list, constants):
-    """Gives the latex code version of remaining
-       determining equation.
+    """Gives the latex code version of remaining determining equation.
 
-       Args:
-       det_eqn (dict): dictionary will all the
-                       determining equations. 
+    Parameters
+    ----------
+    det_eqn : [type]
+        [description
+    var_dict : [dictionary]
+        dictionary mapping the name of the variable to its latex code
+    var_list : [list]
+        list with all independent and dependant variables in string format
+    constants : [list]
+        list containing all constants
+
+    Returns
+    -------
+    [string]
+        single string with all the equations typed in latex format
     """
     latex_code = ''
     for eqn in det_eqn.values():
-        latex_code = latex_code + latex_eqn_code(eqn,
-                                                 var_dict, var_list, constants) + "\\" + "\\" + "\n"
+        latex_code += latex_eqn_code(eqn, var_dict, var_list, constants) + \
+            "\\" + "\\" + "\n"
     return latex_code
